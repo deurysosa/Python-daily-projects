900de3e7252925836838245e9e9690ec5d3b2c9f
PyDeu <deurisosa051@gmail.com>
2025-01-26 20:54:18 -0500

    Adding two numbers #2
    Hello fellas, here a program where you may add two numbers or subtract, multiply, and divide.

---
+++ Adding_Two_Numbers.py
@@ -0,0 +1,14 @@
+#Hello today I will be solving the problem of adding two numbers together.
+
+#The problem is as follows:
+num_1 = float(input("Enter the first number: "))
+num_2 = float(input("Enter the second number: "))
+
+#The sum of the two numbers is calculated by adding the two numbers together.
+sum = num_1 + num_2
+print("The sum of the two numbers is: ", sum)
+
+#The code above is a simple code that adds two numbers together.
+#The code first asks the user to input two numbers. 
+#The numbers are then added together and the sum is printed out.
+#Also you can subtract, multiply, and divide the two numbers by changing the operator in the sum variable.
